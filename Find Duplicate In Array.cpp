c5b8152fa05e4e343cae313c722778803985195d
dhanendraverma <46896989+dhanendraverma@users.noreply.github.com>
2019-06-07 23:50:05 +0530

    Add files via upload

--- Find Duplicate In Array.cpp
+++ Find Duplicate In Array.cpp
@@ -1,10 +1,10 @@
 int Solution::repeatedNumber(const vector<int> &A) {
-    vector <bool> check(A.size(),false);
+    vector <bool> check(A.size(),false); //creating a boolean vector to keep tracked of seen intergers
 
     for(int i=0;i<A.size();i++){
-        if(!check[A[i]])
+        if(!check[A[i]]) //if element is seen first time marked it as seen
             check[A[i]]=true;
-        else
+        else                //else it has been already
             return A[i];
     }
 }
