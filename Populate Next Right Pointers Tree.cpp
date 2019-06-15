a3be1f20456e75e0c4696f0bef12d9af3644a6a1
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-06-15 23:59:16 +0530

    Update Populate Next Right Pointers Tree.cpp

--- Populate Next Right Pointers Tree.cpp
+++ Populate Next Right Pointers Tree.cpp
@@ -19,26 +19,26 @@ void Solution::connect(TreeLinkNode* A) {
     A->next = NULL;
     while (A)
     {
-        TreeLinkNode* B = A;
-        while (B)
+        TreeLinkNode* temp = A;
+        while (temp) //we traverse this temp pointer using next pointer so reccur till given level is not done 
         {
-            if (B->left)
+            if (temp->left) //if the node at current level has left child
             {
-                if (B->right)
-                    B->left->next = B->right;
+                if (temp->right) //then set it's left child's next pointer to the right available node which 
+                                //would be current's right (if exist)
+                    temp->left->next = temp->right; 
                 else
-                    B->left->next = nextRight(B);
+                    temp->left->next = nextRight(temp); //if right child is not present then find which node is present in immediate right
             }
-            if (B->right)
-                B->right->next = nextRight(B);
+            if (temp->right) //if right is there set it's next pointer too
+                temp->right->next = nextRight(temp);
                 
-            B = B->next;
+            temp = temp->next; //this helps in traversing the given level as this level has already set it's next pointer 
         }
-        if (A->left)
+        if (A->left)//when level is done reccur for next level
             A = A->left;
         else if (A->right)
             A = A->right;
         else
             A = nextRight(A);
     }
-}
