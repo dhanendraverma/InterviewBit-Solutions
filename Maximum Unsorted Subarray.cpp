00b1fb280d93173d369df5b356ef04df85ea53db
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-06-23 00:06:49 +0530

    Update Maximum Unsorted Subarray.cpp

--- Maximum Unsorted Subarray.cpp
+++ Maximum Unsorted Subarray.cpp
@@ -1,45 +1,44 @@
-
 vector<int> Solution::subUnsort(vector<int> &A) {
-     int n=A.size();
-    vector<int>res;
-    int s=-1,e=-1;
-    for(int i=0;i<n-1;i++){
+    int n=A.size();
+    vector<int> ans;
+    int s = -1,e = -1;
+    for(int i=0;i<n-1;i++) {
         if(A[i]>A[i+1]){
-            s=i;
+            s=i+1;
             break;
         }
     }
-    if(s==-1){
-        res.push_back(-1);
-        return res;
+    
+    if(s == -1) {
+        ans.push_back(-1);
+        return ans;
     }
-    for(int i=n-1;i>0;i--){
-        if(A[i]<A[i-1]){
+    for(int i=n-1; i>0; i--){
+        if(A[i]<A[i-1]) {
             e=i;
             break;
         }
     }
-   // cout<<s<<" "<<e<<endl;
-    int minm=A[s],maxm=A[s];
-    for(int i=s+1;i<=e;i++){
-        if(A[i]<minm)
-         minm=A[i];
-        else if(A[i]>maxm)
-         maxm=A[i];
+  
+    int mine = A[s], maxe = A[s];
+    
+    for(int i=s+1;i<=e;i++) {
+        mine = min(mine, A[i]);
+        maxe = max(maxe, A[i]);
     }
-    for(int i=0;i<s;i++){
-        if(A[i]>minm){
+    for(int i=0;i<s;i++) {
+        if(A[i]>mine){
          s=i;
          break;
         }
     }
-    for(int i=n-1;i>e;i--){
-        if(A[i]<maxm){
+    for(int i=n-1;i>e;i--) {
+        if(A[i]<maxe){
          e=i;
          break;
         }
     }
-    res.push_back(s);
-    res.push_back(e);
-    return res;
+    ans.push_back(s);
+    ans.push_back(e);
+    return ans;
 }
