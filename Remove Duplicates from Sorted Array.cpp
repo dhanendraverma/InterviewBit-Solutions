07f94a4800227668188d3ce4db1e9b6cac37f4ad
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-07-06 01:09:06 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,22 +1,37 @@
+/****************************************************************************************************************************************
+Remove duplicates from Sorted Array
+Given a sorted array, remove the duplicates in place such that each element appears only once and return the new length.
+
+Note that even though we want you to return the new length, make sure to change the original array as well in place
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+Example: 
+Given input array A = [1,1,2],
+Your function should return length = 2, and A is now [1,2]. 
+-----------------------------------------------------------------------------------------------------------------------------------------
+Approach:
+Travese the array using the two pointers i and j.i starts from 0th and j 1st. Now check if element at the two indexes are not equal then 
+the element at the jth index must come after the index i hence incease the ith by one and store element there now if two elements are eual
+in such case leave jth as it's as we already have a copy at ith index, increase jth index and check for next elements.At last when jth
+reached the last we can delete the elements from (j+1) to last as this are these contain duplicates only.
+*****************************************************************************************************************************************/
+
 int Solution::removeDuplicates(vector<int> &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    
     if(A.size() == 0){
         return A.size();
     }
     
-    int k = 0;
-    for(int i = 1; i < A.size(); i++){
-        if(A[k] != A[i]){
-            A[k+1] = A[i];
-            k++;
+    int i=0,j=1;
+    while(j<A.size()){
+        if(A[i] != A[j]) {
+            i++;
+            A[i] = A[j];
+            
         }
+        j++;
     }
     
-    A.erase(A.begin()+k+1, A.end());
-    return k+1;
+    A.erase(A.begin()+i+1, A.end());
+    return i+1;
     
 }
