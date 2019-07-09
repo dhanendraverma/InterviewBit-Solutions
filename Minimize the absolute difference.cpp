4b63af3012cd59eb9273b124a1be36f7e469e249
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-07-09 12:22:42 +0530

    Update Minimize the absolute difference.cpp

--- Minimize the absolute difference.cpp
+++ Minimize the absolute difference.cpp
@@ -18,10 +18,10 @@ Explanation: We get the minimum difference for a=5, b=6, c=6 as | max(a,b,c) - m
 Approach:
 We have to minimize |max(a,b,c)-min(a,b,c)| in order to do so there are two ways value of max(a,b,c) should be minimum possible and 
 value of min(a,b,c) should be maximum possible which would cause the difference of the two to get reduced. Now as given arrays are 
-sorted we start from the largest possible values of a,b,c which are the last elements of each array now at every step he possible ways
-to decrease the differece is to decrease maximum element out of all three so we check which array is presently giving the max element
+sorted we start from the largest possible values of a,b,c which are the last elements of each array now at every step the possible ways
+to decrease the differece is to decrease maximum element out of all three, so we check which array is presently giving the max element
 and get the next lower element from that array by reducing its index value. Keep repeating untill one of the any run out of elements
-every update the ans.
+at every stage keep updating the ans.
 ***************************************************************************************************************************************/
 
 int Solution::solve(vector<int> &A, vector<int> &B, vector<int> &C) {
