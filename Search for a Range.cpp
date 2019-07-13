748a111f5c33c7d9fee2332197b18b613d9cf4eb
Dhanendra Verma <46896989+dhanendraverma@users.noreply.github.com>
2019-07-13 22:38:52 +0530

    Create Search for a Range.cpp

---
+++ Search for a Range.cpp
@@ -0,0 +1,32 @@
+vector<int> Solution::searchRange(const vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+     int n=A.size();
+    int low=0,high=n-1;
+    int mid=low+(high-low)/2;
+    vector<int>v(2);
+    v[0]=-1;v[1]=-1;
+    while(low<=high){
+        mid=low+(high-low)/2;
+        if(A[mid]==B){
+             v[0]=mid;
+             high=mid-1;
+        }
+        else if(A[mid]<B) low=mid+1;
+        else high=mid-1;
+    }
+    if(v[0]==-1) return v;
+    low=v[0];high=n-1;
+    while(low<=high){
+        mid=low+(high-low)/2;
+        if(A[mid]==B){
+             v[1]=mid;
+             low=mid+1;
+        }
+        else if(A[mid]<B) low=mid+1;
+        else high=mid-1;
+    }
+    return v;
+}
